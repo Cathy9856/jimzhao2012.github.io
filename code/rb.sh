2927dcdeaca5a646338ecef23be6df9fd8f19a7c
jimzhao2012 <jimzhao@DST56065-1.local>
2014-01-02 16:09:09 +0800

    modify js lib

--- code/rb.sh
+++ code/rb.sh
@@ -8,4 +8,15 @@ tar -cvzf bridge.min.zip  bridge.min.js;
 cp bridge.js demo/bridge.js;
 rm demo.zip;
 tar -cvzf demo.zip demo;
-
+rm -rf tb;
+mkdir tb;
+cp bridge.js tb;
+cd tb;
+rm -rf ../../api;
+yuidoc . -o ../../api;
+cd ..;
+rm -rf tb;
+cd ..;
+git add --all;
+git commit -m "modify js lib";
+git push;
