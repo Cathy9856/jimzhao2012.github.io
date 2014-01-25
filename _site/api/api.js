408558add9a6196536e3b7e2de27d001dd26f614
jimzhao <jimzhao2012@gmail.com>
2014-01-25 23:12:32 +0800

    modify

---
+++ _site/api/api.js
@@ -0,0 +1,13 @@
+YUI.add("yuidoc-meta", function(Y) {
+   Y.YUIDoc = { meta: {
+    "classes": [
+        "CtripEncrypt",
+        "CtripTool",
+        "CtripUser",
+        "CtripUtil",
+        "Internal"
+    ],
+    "modules": [],
+    "allModules": []
+} };
+});
---
+++ _site/sc
@@ -0,0 +1,2 @@
+rm -rf api; yuidoc --themedir yui_theme --outdir api .;
+
