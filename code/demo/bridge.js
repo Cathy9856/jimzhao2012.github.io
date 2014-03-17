829d249be264b170d9d0b54cba6c1515d12e0a45
jimzhao2012 <jimzhao2012@gmail.com>
2014-03-17 19:37:01 +0800

    modify js lib

--- code/demo/bridge.js
+++ code/demo/bridge.js
@@ -2403,7 +2403,7 @@ var CtripBar = {
         //导航栏总共分为3部分，1.左侧，返回按钮，不能修改; 2. 中间title，可以任意设置; 3.右侧按钮，定义格式为{tagname:"xxxx",value:"btn_title"}
         var nav_json = {
             "center": [{"tagname": "title", "value":"携程"},{"tagname":"subtitle", value:"上海到北京"}],
-            "centerButton": [{"tagname": "cityChoose", "value":"上海", "a_icon":"arrow_up.png", "i_icon":"arrow_up.png"}], //from 5.5version
+            "centerButton": [{"tagname": "cityChoose", "value":"上海", "a_icon":"arrow_up", "i_icon":"arrow_up.png"}], //from 5.5version
             "right": [{"tagname": "click_tag_name", "value":"Click"}]
         }
         //nav_json参数配置说明：
