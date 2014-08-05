e318146f740b8b64eac072ef987e19b18779b827
jim <jim@dst60174.local>
2014-08-05 22:01:42 +0800

    modify js lib

---
+++ api/api.js
@@ -0,0 +1,21 @@
+YUI.add("yuidoc-meta", function(Y) {
+   Y.YUIDoc = { meta: {
+    "classes": [
+        "CtripBar",
+        "CtripBusiness",
+        "CtripEncrypt",
+        "CtripFile",
+        "CtripMap",
+        "CtripPage",
+        "CtripPay",
+        "CtripPipe",
+        "CtripSumSungWallet",
+        "CtripTool",
+        "CtripUser",
+        "CtripUtil",
+        "Internal"
+    ],
+    "modules": [],
+    "allModules": []
+} };
+});
