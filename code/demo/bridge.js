61ed32ee94f7c1d73d1f98fad49f2f7038d0579a
jim <jim@dst60174.cn1.global.ctrip.com>
2015-01-15 18:16:26 +0800

    modify js lib

--- code/demo/bridge.js
+++ code/demo/bridge.js
@@ -2,11 +2,9 @@
 var __CTRIP_JS_PARAM = "?jsparam="
 var __CTRIP_URL_PLUGIN = "ctrip://h5/plugin" + __CTRIP_JS_PARAM;
 var __CTRIP_YOUTH_URL_PLUGIN = "ctripyouth://h5/plugin" + __CTRIP_JS_PARAM;
-
-
 var base64EncodeChars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/";
 var base64DecodeChars = new Array(-1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1,-1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, 62, -1, -1, -1, 63, 52, 53, 54, 55, 56, 57, 58, 59, 60, 61, -1, -1, -1, -1, -1, -1, -1,  0,  1,  2,  3,  4,  5,  6,  7,  8,  9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, -1, -1, -1, -1, -1, -1, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, -1, -1, -1, -1, -1);
-
+var __Is_Base64_Encode_Version = false;
 
 /**
 * @class Internal
@@ -316,7 +314,7 @@ var CtripConsole = {
  */
 function __bridge_callback(param) {
     param = CtripTool.base64Decode(param);
-    console.log("callback param::["+param+"]");
+//    console.log("callback param::["+param+"]");
     var jsonObj = JSON.parse(param);
 
     if (jsonObj != null) {
@@ -558,7 +556,7 @@ var CtripTool = {
      * @description Base64 encode(UTF8编码)，JS实现
      * Base64 encode(UTF8编码)，JS实现
      * @brief Base64 encode(UTF8编码)，JS实现，立即返回数据
-     * @method base64encode
+     * @method base64Encode
      * @since version 6.2
      * @author jimzhao
      * @example
