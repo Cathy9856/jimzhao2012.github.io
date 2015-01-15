94aa3f7999c588b6af40de1af48222fdce5eb2ba
jim <jim@dst60174.cn1.global.ctrip.com>
2015-01-15 16:59:55 +0800

    modify js lib

--- code/bridge.js
+++ code/bridge.js
@@ -136,119 +136,151 @@ var Base64 = ( function( ) {
 }( ) );
 
 
+var base64DecodeChars = new Array(
+        -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1,
+        -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1,
+        -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, 62, -1, -1, -1, 63,
+       52, 53, 54, 55, 56, 57, 58, 59, 60, 61, -1, -1, -1, -1, -1, -1,
+       -1,  0,  1,  2,  3,  4,  5,  6,  7,  8,  9, 10, 11, 12, 13, 14,
+       15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, -1, -1, -1, -1, -1,
+       -1, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40,
+       41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, -1, -1, -1, -1, -1);
+   //编码的方法
+   function base64encode(str) {
+       var out, i, len;
+       var c1, c2, c3;
+       len = str.length;
+       i = 0;
+       out = "";
+       while(i < len) {
+       c1 = str.charCodeAt(i++) & 0xff;
+       if(i == len)
+       {
+           out += base64EncodeChars.charAt(c1 >> 2);
+           out += base64EncodeChars.charAt((c1 & 0x3) << 4);
+           out += "==";
+           break;
+       }
+       c2 = str.charCodeAt(i++);
+       if(i == len)
+       {
+           out += base64EncodeChars.charAt(c1 >> 2);
+           out += base64EncodeChars.charAt(((c1 & 0x3)<< 4) | ((c2 & 0xF0) >> 4));
+           out += base64EncodeChars.charAt((c2 & 0xF) << 2);
+           out += "=";
+           break;
+       }
+       c3 = str.charCodeAt(i++);
+       out += base64EncodeChars.charAt(c1 >> 2);
+       out += base64EncodeChars.charAt(((c1 & 0x3)<< 4) | ((c2 & 0xF0) >> 4));
+       out += base64EncodeChars.charAt(((c2 & 0xF) << 2) | ((c3 & 0xC0) >>6));
+       out += base64EncodeChars.charAt(c3 & 0x3F);
+       }
+       return out;
+   }
+   //解码的方法
+   function base64decode(str) {
+       var c1, c2, c3, c4;
+       var i, len, out;
+       len = str.length;
+       i = 0;
+       out = "";
+       while(i < len) {
+       
+       do {
+           c1 = base64DecodeChars[str.charCodeAt(i++) & 0xff];
+       } while(i < len && c1 == -1);
+       if(c1 == -1)
+           break;
+       
+       do {
+           c2 = base64DecodeChars[str.charCodeAt(i++) & 0xff];
+       } while(i < len && c2 == -1);
+       if(c2 == -1)
+           break;
+       out += String.fromCharCode((c1 << 2) | ((c2 & 0x30) >> 4));
+       
+       do {
+           c3 = str.charCodeAt(i++) & 0xff;
+           if(c3 == 61)
+           return out;
+           c3 = base64DecodeChars[c3];
+       } while(i < len && c3 == -1);
+       if(c3 == -1)
+           break;
+       out += String.fromCharCode(((c2 & 0XF) << 4) | ((c3 & 0x3C) >> 2));
+       
+       do {
+           c4 = str.charCodeAt(i++) & 0xff;
+           if(c4 == 61)
+           return out;
+           c4 = base64DecodeChars[c4];
+       } while(i < len && c4 == -1);
+       if(c4 == -1)
+           break;
+       out += String.fromCharCode(((c3 & 0x03) << 6) | c4);
+       }
+       return out;
+   }
+   function utf16to8(str) {
+       var out, i, len, c;
+       out = "";
+       len = str.length;
+       for(i = 0; i < len; i++) {
+       c = str.charCodeAt(i);
+       if ((c >= 0x0001) && (c <= 0x007F)) {
+           out += str.charAt(i);
+       } else if (c > 0x07FF) {
+           out += String.fromCharCode(0xE0 | ((c >> 12) & 0x0F));
+         out += String.fromCharCode(0x80 | ((c >>  6) & 0x3F));
+         out += String.fromCharCode(0x80 | ((c >>  0) & 0x3F));
+     } else {
+         out += String.fromCharCode(0xC0 | ((c >>  6) & 0x1F));
+         out += String.fromCharCode(0x80 | ((c >>  0) & 0x3F));
+     }
+     }
+     return out;
+ }
+ function utf8to16(str) {
+     var out, i, len, c;
+     var char2, char3;
+     out = "";
+     len = str.length;
+     i = 0;
+     while(i < len) {
+     c = str.charCodeAt(i++);
+     switch(c >> 4)
+     { 
+       case 0: case 1: case 2: case 3: case 4: case 5: case 6: case 7:
+         // 0xxxxxxx
+         out += str.charAt(i-1);
+         break;
+       case 12: case 13:
+         // 110x xxxx   10xx xxxx
+         char2 = str.charCodeAt(i++);
+         out += String.fromCharCode(((c & 0x1F) << 6) | (char2 & 0x3F));
+         break;
+       case 14:
+         // 1110 xxxx  10xx xxxx  10xx xxxx
+         char2 = str.charCodeAt(i++);
+         char3 = str.charCodeAt(i++);
+         out += String.fromCharCode(((c & 0x0F) << 12) |
+                        ((char2 & 0x3F) << 6) |
+                        ((char3 & 0x3F) << 0));
+         break;
+     }
+     }
+     return out;
+ }
+ 
+ function xxencode(src) {
+    return base64encode(utf16to8(src));
+ }
 
-    var error = function(message) {
-        // Note: the error messages used throughout this file match those used by
-        // the native `atob`/`btoa` implementation in Chromium.
-        // throw new InvalidCharacterError(message);
-        console.log("ERROR:::"+error);
-    };
-
-    var TABLE = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/';
-    // http://whatwg.org/html/common-microsyntaxes.html#space-character
-    var REGEX_SPACE_CHARACTERS = /[\t\n\f\r ]/g;
-
-    // `decode` is designed to be fully compatible with `atob` as described in the
-    // HTML Standard. http://whatwg.org/html/webappapis.html#dom-windowbase64-atob
-    // The optimized base64-decoding algorithm used is based on @atk’s excellent
-    // implementation. https://gist.github.com/atk/1020396
-    var xxdecode = function(input) {
-        input = String(input)
-            .replace(REGEX_SPACE_CHARACTERS, '');
-        var length = input.length;
-        if (length % 4 == 0) {
-            input = input.replace(/==?$/, '');
-            length = input.length;
-        }
-        if (
-            length % 4 == 1 ||
-            // http://whatwg.org/C#alphanumeric-ascii-characters
-            /[^+a-zA-Z0-9/]/.test(input)
-        ) {
-            error(
-                'Invalid character: the string to be decoded is not correctly encoded.'
-            );
-        }
-        var bitCounter = 0;
-        var bitStorage;
-        var buffer;
-        var output = '';
-        var position = -1;
-        while (++position < length) {
-            buffer = TABLE.indexOf(input.charAt(position));
-            bitStorage = bitCounter % 4 ? bitStorage * 64 + buffer : buffer;
-            // Unless this is the first of a group of 4 characters…
-            if (bitCounter++ % 4) {
-                // …convert the first 8 bits to a single ASCII character.
-                output += String.fromCharCode(
-                    0xFF & bitStorage >> (-2 * bitCounter & 6)
-                );
-            }
-        }
-        return output;
-    };
-
-    // `encode` is designed to be fully compatible with `btoa` as described in the
-    // HTML Standard: http://whatwg.org/html/webappapis.html#dom-windowbase64-btoa
-    var xxencode = function(input) {
-        input = String(input);
-        if (/[^\0-\xFF]/.test(input)) {
-            // Note: no need to special-case astral symbols here, as surrogates are
-            // matched, and the input is supposed to only contain ASCII anyway.
-            error(
-                'The string to be encoded contains characters outside of the ' +
-                'Latin1 range.'
-            );
-        }
-        var padding = input.length % 3;
-        var output = '';
-        var position = -1;
-        var a;
-        var b;
-        var c;
-        var d;
-        var buffer;
-        // Make sure any padding is handled outside of the loop.
-        var length = input.length - padding;
-
-        while (++position < length) {
-            // Read three bytes, i.e. 24 bits.
-            a = input.charCodeAt(position) << 16;
-            b = input.charCodeAt(++position) << 8;
-            c = input.charCodeAt(++position);
-            buffer = a + b + c;
-            // Turn the 24 bits into four chunks of 6 bits each, and append the
-            // matching character for each of them to the output.
-            output += (
-                TABLE.charAt(buffer >> 18 & 0x3F) +
-                TABLE.charAt(buffer >> 12 & 0x3F) +
-                TABLE.charAt(buffer >> 6 & 0x3F) +
-                TABLE.charAt(buffer & 0x3F)
-            );
-        }
-
-        if (padding == 2) {
-            a = input.charCodeAt(position) << 8;
-            b = input.charCodeAt(++position);
-            buffer = a + b;
-            output += (
-                TABLE.charAt(buffer >> 10) +
-                TABLE.charAt((buffer >> 4) & 0x3F) +
-                TABLE.charAt((buffer << 2) & 0x3F) +
-                '='
-            );
-        } else if (padding == 1) {
-            buffer = input.charCodeAt(position);
-            output += (
-                TABLE.charAt(buffer >> 2) +
-                TABLE.charAt((buffer << 4) & 0x3F) +
-                '=='
-            );
-        }
-
-        return output;
-    };
-
+ function xxdecode(src) {
+    return utf8to16(base64decode(src));
+ }
+ 
 /**
 * @class Internal
 * @description bridge.js内部使用的工具类
