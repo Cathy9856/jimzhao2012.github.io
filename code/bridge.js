a48bd844172d258b1e46b70de062f6e31db75b6b
jim <jim@dst60174.cn1.global.ctrip.com>
2015-01-15 16:42:42 +0800

    modify js lib

--- code/bridge.js
+++ code/bridge.js
@@ -135,6 +135,120 @@ var Base64 = ( function( ) {
       
 }( ) );
 
+
+
+    var error = function(message) {
+        // Note: the error messages used throughout this file match those used by
+        // the native `atob`/`btoa` implementation in Chromium.
+        // throw new InvalidCharacterError(message);
+        console.log("ERROR:::"+error);
+    };
+
+    var TABLE = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/';
+    // http://whatwg.org/html/common-microsyntaxes.html#space-character
+    var REGEX_SPACE_CHARACTERS = /[\t\n\f\r ]/g;
+
+    // `decode` is designed to be fully compatible with `atob` as described in the
+    // HTML Standard. http://whatwg.org/html/webappapis.html#dom-windowbase64-atob
+    // The optimized base64-decoding algorithm used is based on @atk’s excellent
+    // implementation. https://gist.github.com/atk/1020396
+    var xxdecode = function(input) {
+        input = String(input)
+            .replace(REGEX_SPACE_CHARACTERS, '');
+        var length = input.length;
+        if (length % 4 == 0) {
+            input = input.replace(/==?$/, '');
+            length = input.length;
+        }
+        if (
+            length % 4 == 1 ||
+            // http://whatwg.org/C#alphanumeric-ascii-characters
+            /[^+a-zA-Z0-9/]/.test(input)
+        ) {
+            error(
+                'Invalid character: the string to be decoded is not correctly encoded.'
+            );
+        }
+        var bitCounter = 0;
+        var bitStorage;
+        var buffer;
+        var output = '';
+        var position = -1;
+        while (++position < length) {
+            buffer = TABLE.indexOf(input.charAt(position));
+            bitStorage = bitCounter % 4 ? bitStorage * 64 + buffer : buffer;
+            // Unless this is the first of a group of 4 characters…
+            if (bitCounter++ % 4) {
+                // …convert the first 8 bits to a single ASCII character.
+                output += String.fromCharCode(
+                    0xFF & bitStorage >> (-2 * bitCounter & 6)
+                );
+            }
+        }
+        return output;
+    };
+
+    // `encode` is designed to be fully compatible with `btoa` as described in the
+    // HTML Standard: http://whatwg.org/html/webappapis.html#dom-windowbase64-btoa
+    var xxencode = function(input) {
+        input = String(input);
+        if (/[^\0-\xFF]/.test(input)) {
+            // Note: no need to special-case astral symbols here, as surrogates are
+            // matched, and the input is supposed to only contain ASCII anyway.
+            error(
+                'The string to be encoded contains characters outside of the ' +
+                'Latin1 range.'
+            );
+        }
+        var padding = input.length % 3;
+        var output = '';
+        var position = -1;
+        var a;
+        var b;
+        var c;
+        var d;
+        var buffer;
+        // Make sure any padding is handled outside of the loop.
+        var length = input.length - padding;
+
+        while (++position < length) {
+            // Read three bytes, i.e. 24 bits.
+            a = input.charCodeAt(position) << 16;
+            b = input.charCodeAt(++position) << 8;
+            c = input.charCodeAt(++position);
+            buffer = a + b + c;
+            // Turn the 24 bits into four chunks of 6 bits each, and append the
+            // matching character for each of them to the output.
+            output += (
+                TABLE.charAt(buffer >> 18 & 0x3F) +
+                TABLE.charAt(buffer >> 12 & 0x3F) +
+                TABLE.charAt(buffer >> 6 & 0x3F) +
+                TABLE.charAt(buffer & 0x3F)
+            );
+        }
+
+        if (padding == 2) {
+            a = input.charCodeAt(position) << 8;
+            b = input.charCodeAt(++position);
+            buffer = a + b;
+            output += (
+                TABLE.charAt(buffer >> 10) +
+                TABLE.charAt((buffer >> 4) & 0x3F) +
+                TABLE.charAt((buffer << 2) & 0x3F) +
+                '='
+            );
+        } else if (padding == 1) {
+            buffer = input.charCodeAt(position);
+            output += (
+                TABLE.charAt(buffer >> 2) +
+                TABLE.charAt((buffer << 4) & 0x3F) +
+                '=='
+            );
+        }
+
+        return output;
+    };
+
 /**
 * @class Internal
 * @description bridge.js内部使用的工具类
@@ -360,7 +474,7 @@ var Internal = {
             paramString = "";
         }
 
-        paramString = Base64.encode(paramString);
+        paramString = xxencode(paramString);
         if (Internal.isYouthApp) {
             return __CTRIP_YOUTH_URL_PLUGIN + paramString;
         } else {
@@ -378,47 +492,6 @@ var Internal = {
     callWin8App:function(paramString) {
         window.external.notify(paramString);
     }
-
-//     execAPI:function(supportVersion, modelName, actionName, params, callbackTagName) {
-//         console.log("start exec execAPIA");
-//         if ((supportVersion != null) && !Internal.isSupportAPIWithVersion(supportVersion)) {
-//             return;
-//         }
-// //        Internal.execAPI("5.4","NavBar", "setNavBarHidden",params,"set_navbar_hidden");
-
-//         paramString = Internal.makeParamString(modelName, actionName, params, callbackTagName);
-//         console.log("start exec execAPIB:" + paramString);
-
-//         if (Internal.isIOS) {
-//             url = Internal.makeURLWithParam(paramString);
-//             Internal.loadURL(url);
-//         }
-//         else if(Internal.isAndroid) {
-//             try {
-//                 var pluginModelName = modelName + "_a";
-//                 var pluginCmd = window[pluginModelName];
-//                 if (pluginCmd != null) {
-//                     pluginCmd = pluginCmd[actionName];
-//                     console.log("start exec execAPID:" + pluginCmd);
-
-//                     if (pluginCmd != null) {
-//                         console.log("start exec execAPIE:" + pluginCmd);
-//                         //pluginCmd=window.Util_a.setNavBarHidden
-//                         vard = pluginCmd(paramString);
-//                         console.log("start exec vard:" + vard);
-//                         eval(vard);      
-//                         console.log("start exec execAPIF:" + pluginCmd);
-                  
-//                     }
-//                 }
-//             } catch(e) {
-//                  console.log("start exec ErrorG:" + e);
-//             }
-//         }
-//         else if (Internal.isWinOS) {
-//                 Internal.callWin8App(paramString);
-//         }
-//     }
 };
 
 var originalConsole = console;
@@ -483,7 +556,7 @@ var CtripConsole = {
  * @author jimzhao
  */
 function __bridge_callback(param) {
-    param = Base64.decode(param);
+    param = xxdecode(param);
     var jsonObj = JSON.parse(param);
 
     if (jsonObj != null) {
